99e21babb265c4b19611e657b7e111a43ca239d7
Yousef Aldawoud <yousef.l.t.aldawoud@gmail.com>
2019-07-10 02:39:23 +1200

    Made a 404 page

---
+++ src/404.php
@@ -0,0 +1,17 @@
+<?
+set_include_path("var/www/html/");
+ ?>
+<? include('views/main-layout.php');?>
+    <body>
+        <?php include('views/navbar.php');?>
+        <div class="row center">
+            <div class="card sm6 md4 lg3" style="background:white">
+                <div class="card-title">404 page not found</div>
+                <div class="card-content">
+                    <a href="/">return back to the main page</a>
+                </div>
+            </div>
+        </div>
+        <script src="js/navbar.js"></script>
+    </body>
+</html> 
