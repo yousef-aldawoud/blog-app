fe6e927db5d9c0c73f6a780f87f0bbd0119c36d3
Yousef Aldawoud <yousef.l.t.aldawoud@gmail.com>
2019-07-13 02:08:39 +1200

    Fixed login page

--- src/login.php
+++ src/login.php
@@ -1,4 +1,4 @@
-<?
+<?php
 session_start();
 
 set_include_path(getenv("INCLUDE_PATH"));
@@ -6,8 +6,7 @@ require_once("middlewares.php");
 require_once("Models/Help.php");
 
  ?>
-<? include('views/main-layout.php');?>
-<body>
+<?php include('views/main-layout.php');?>
         <?php include('views/navbar.php');?>
         <div class="container">
             <div class="card login-card">
@@ -18,29 +17,28 @@ require_once("Models/Help.php");
 
                     <form action="/user.php" method="post" class="row center wrap">
                         <input placeholder="Username" type="text" class="text-field" name="username" >
-                        <input  type="hidden" class="text-field" name="_token" value="<?echo $token;?>">
+                        <input  type="hidden" class="text-field" name="_token" value="<?php echo $token;?>">
                         <input  type="hidden" class="text-field" name="route" value="login">
                         <input placeholder="Password" type="password" name="password" class="text-field">
                         <button class="login-btn">LOGIN</button>
                     </form>
-                    <?if (isset($_SESSION['errors'])): ?>
+                    <?php if (isset($_SESSION['errors'])){ ?>
                         <div class="error">
                         <ul>
-                        <? foreach($_SESSION['errors'] as $error):?>
+                        <?php foreach($_SESSION['errors'] as $error){?>
 
                         <li>
-                            <? echo $error; ?>
+                            <?php echo $error; ?>
                         </li>
-                        <?
-                            endforeach;
+                        <?php
+                        }
                             unset($_SESSION['errors']);
                         ?>
                         </ul>
                         </div>
-                    <?endif;?>
+                        <?php }?>
                 </div> 
             </div>
         </div>
         <script src="js/navbar.js"></script>
-    </body>
-</html> 
+    
